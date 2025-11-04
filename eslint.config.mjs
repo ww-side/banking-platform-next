6464097f917f85232fbcbda3c5f2348422802f74
ww-side <daniil.metelia@gmail.com>
2025-11-04 20:42:58 +0200

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^next', '^@?\\w'],
+            ['^@/app'],
+            ['^@/core'],
+            ['^@/features'],
+            ['^$'],
+            ['^@/shared'],
+            ['^'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports' },
+      ],
+    },
+  },
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+]);
+
+export default eslintConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import { cn } from '@/shared/lib/utils/cn';
+
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(geistSans.variable, geistMono.variable, 'antialiased')}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
